b09afe74f8db658fbf4316b596e1a21e281b9382
saurabhvaj <saurabhvajpayee2010@gmail.com>
2017-05-02 13:20:54 +0530

    updated deploySSHKeyIntoBitbucket.sh to get variables from property file

--- files/scripts/deploySSHKeyIntoBitbucket.sh
+++ files/scripts/deploySSHKeyIntoBitbucket.sh
@@ -1,37 +1,42 @@
 #!/bin/bash
 
+. /var/lib/jenkins/scripts/project.envs
+ 
 repoName=$1
 deployKey=$2
-baseURL="https://api.bitbucket.org/"
+baseURL=$baseURL
+#apiVersion=$apiVersion
 apiVersion="1.0"
-username="username"
-password="password"
-organization="yourorgname"
-jenkinsHome="/var/lib/jenkins"
-keyName="id_rsa.pub"
+username=$username
+password=$password
+organization=$organization
+jenkinsHome=$jenkinsHome
+keyName=$keyName
+
+
+function deployKey(){
+
+repoName=$1
+deployKey=$2
+
+
+        curl -s -X POST -u ${username}:${password} "${baseURL}/${apiVersion}/repositories/${organization}/${repoName}/deploy-keys" --data-urlencode "key=${keyFileContent}"
+}
 
 
 if [ $deployKey != null ]; then
 	
-	keyFileContent=$(cat ${jenkinsHome}/workspace/deployKey/${keyName})
+	keyFileContent=$(cat ${jenkinsHome}/workspace/DevopsDeploySSHKey/${keyName})
 	deployKey ${repoName} ${keyFileContent}
 
-elif
+else
 	if [[ -z ${jenkinsHome}/.ssh/id_rsa.pub ]]; then
 		
 		keyFileContent=$(cat ${jenkinsHome}/.ssh/${keyName})
 		deployKey ${repoName} ${keyFileContent}
-	elif
+	else
 		ssh-keygen -f id_rsa -t rsa -N ''
 		keyFileContent=$(cat ${jenkinsHome}/.ssh/${keyName})
                 deployKey ${repoName} ${keyFileContent}
+	fi
 fi
-
-
-deployKey(){
-
-repoName=$1
-deployKey=$2
-
-	curl -s -X POST -u ${username}:${password} "${baseURL}/${apiVersion}/repositories/${organization}/${repoName}/deploy-keys" --data-urlencode "key=${keyFileContent}"
-}
